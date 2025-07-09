35d6ca96f90dafeb59b098963b72cdb46f18c3ba
mataillade <matias.taillade@etu.u-bordeaux.fr>
2025-07-09 19:16:28 +0200

    template add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import { ScrollToTop } from './components/common/ScrollToTop';
+import AppLayout from './layout/AppLayout';
 
 function App() {
-
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
-    </>
-  )
+      <>
+        <Router>
+          <ScrollToTop />
+          <Routes>
+            <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          </Routes>
+        </Router>
+      </>
+  );
 }
 
-export default App
+export default App;
